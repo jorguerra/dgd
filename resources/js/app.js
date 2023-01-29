3beb2792a720a7aa483a661bd6d191569d2db53e
Jorge Guerra <jorguerra@gmail.com>
2023-01-29 09:57:51 -0300

    feat: inertia setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { InertiaProgress } from "@inertiajs/progress"
+
+InertiaProgress.init()
+
+createInertiaApp({
+
+  resolve: name => import(`/resources/js/Pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
